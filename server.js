c284c395f1985f254ef01cf859b99351a1abf2b6
Faraz <farazgojali@gmail.com>
2023-02-08 21:30:40 +0500

    passport js

--- server.js
+++ server.js
@@ -3,16 +3,55 @@ const path = require('path');
 const https = require('https');
 const express = require('express');
 const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+
+require('dotenv').config();
 
 const PORT = 3000;
 
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET
+};
+
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET
+}
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+    console.log('Google profile', profile);
+    done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 const app = express();
 
-// app.use(helmet());
+app.use(helmet());
+app.use(passport.initialize());
 
-app.get('/secret', (req, res) => {
-    return res.send('Your personal secret value is 42!')
-})
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true; //TODO
+    if (!isLoggedIn) {
+        return res.status(401).json({
+            error: 'You must log in!',
+        })
+    }
+    next();
+}
+
+app.get('/auth/google', (req, res) => { });
+
+app.get('/auth/google/callback', (req, res) => { });
+
+app.get('/auth/logout', (req, res) => { })
+
+app.get('/secret', checkLoggedIn, (req, res) => {
+    return res.send('Your personal secret value is 42!');
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
